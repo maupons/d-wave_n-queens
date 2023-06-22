3ce76251f0ad6317c3084555c13e0669303c802e
Mauricio Pons <maupons@hotmail.com>
2023-06-22 16:52:18 +0000

    reduced BQM models classical and quantum

---
+++ n_queens10C_rBQM.py
@@ -0,0 +1,292 @@
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+#
+# 
+# N Queen Solution using cell's row & columns
+# 
+# - block diagonals
+# - solution verifier
+# - draw board
+# 
+# - removed qubits for blocked diagonals
+# 
+
+from collections import Counter
+
+import numpy as np
+import matplotlib
+matplotlib.use("agg")    # must select backend before importing pyplot
+import matplotlib.pyplot as plt
+from dimod import BinaryQuadraticModel, ExactSolver
+from dwave.samplers import SimulatedAnnealingSampler, SteepestDescentSolver \
+, TabuSampler, TreeDecompositionSolver, RandomSampler
+from time import time
+import sys
+import datetime as dt
+
+
+def n_queens(n,dp,dm,itr, sampler=None):
+    """Returns a potential solution to the n-queens problem in a dictionary, where
+    the keys are qubit id and value on or off
+
+    Args:
+        n: Number of queens to place.
+
+        sampler: A binary quadratic model sampler. 
+        Can use: 1) LeapHybridSampler
+                 2) DWaveSampler - QPU
+                 3) SimulatedAnnealingSampler
+    """
+
+    # d = len(dp) + len(dm)
+    bqm = BinaryQuadraticModel({}, {}, 0, 'BINARY')
+    w = 2
+
+    for i in range(n**2):
+        ri = i // n
+        ci = i-n*ri
+        if (ri+ci) in dp:
+            continue
+        elif (ri-ci) in dm:
+            continue
+        else:
+            bqm.add_variable(i, -w)
+            
+        for j in range(i):
+            if j not in bqm.linear:
+                continue
+            rj = j // n
+            cj = j-n*rj
+            if rj == ri:
+                bqm.add_interaction(i, j, w)
+            if cj == ci:
+                bqm.add_interaction(i, j, w)
+            if abs(ri-rj) == abs(ci-cj):
+                bqm.add_interaction(i, j, w)
+    # print(bqm)
+    # print('LINEAR')
+    # print(bqm.linear)
+    # print(len(bqm.linear))
+    # print('QUADRATIC')
+    # print(bqm.quadratic)
+    # print(len(bqm.quadratic))
+
+
+    print(f'Classical solver started with {itr} reads...')
+    start_time = time()
+
+    # Exact Solver
+    # sampler = ExactSolver()
+    # sampleset = sampler.sample(bqm)
+
+    # Heuristic  Solvers
+    sampler = SimulatedAnnealingSampler()
+    # sampler = SteepestDescentSolver()
+    # sampler = TabuSampler()
+    # sampler = TreeDecompositionSolver()
+    # sampler = RandomSampler()
+    print(sampler)
+    sampleset = sampler.sample(bqm, num_reads=itr)
+
+    py_time = time()-start_time
+    return sampleset, sampler, py_time
+
+
+def is_valid_solution(n, solution):
+    """Check that solution is valid by making sure all constraints were met.
+
+    Args:
+        n: Number of queens in the problem.
+
+        solution: A dictionary of qubits, key = qubit id, value = 0 or 1
+    """
+    board = np.zeros((n,n))
+    ldp = []                            # Keep track of queens on D+
+    ldm = []                            # Keep track of queens on D-
+
+    for qb,v in solution.items():       # Build & Check diag/anti-diag constraints
+        r = qb // n
+        c = qb-n*r
+        if v:
+            board[r,c] = v
+            dp = r+c
+            dm = r-c
+            if dp not in ldp:
+                ldp.append(dp)
+            else:
+                print(f"D+ diagonal {dp} has more than 1 queen")
+                return False
+            if dm not in ldm:
+                ldm.append(dm)
+            else:
+                print(f"D- diagonal {dm} has more than 1 queen")
+                return False
+
+    for i in range(n):                              # Check row/col constraints
+        sum_row = sum(board[i,:])
+        if sum_row != 1:
+            print(f"Row {i} has {sum_row} queens.")
+            return False
+        sum_col = sum(board[:,i])
+        if sum_col != 1:
+            print(f"Column {i} has {sum_col} queens.")
+            return False
+
+    return True
+
+
+def plot_chessboard(n,queens, dp, dm, ruta):
+    """Create a chessboard with queens using matplotlib. Image is saved
+    in the root directory. Returns the image file name.
+    """
+    chessboard = np.zeros((n,n))
+    chessboard[1::2,0::2] = 1
+    chessboard[0::2,1::2] = 1
+
+    # Adjust fontsize for readability
+    if n <= 10:
+        fontsize = 30
+    elif n <= 20:
+        fontsize = 10
+    else:
+        fontsize = 5
+
+    plt.xticks(np.arange(n))
+    plt.yticks(np.arange(n))
+
+    plt.imshow(chessboard, cmap='binary')
+
+    # Place queens
+    for qb,v in queens.items():
+        y = qb // n
+        x = qb-n*y
+        if v:
+            plt.text(x, y, u"\u2655", fontsize=fontsize, ha='center',
+                        va='center', color='black' if (x - y) % 2 == 0 else 'white')
+
+    # Draw blocked diagonals
+    dp_dict = {}
+    dm_dict = {}
+    for i in range(n**2):
+        y = i // n
+        x = i-n*y
+        if (y+x) in dp:
+            if y+x not in dp_dict:
+                dp_dict[y+x] = [[],[]]
+                dp_dict[y+x][0] = [x]
+                dp_dict[y+x][1] = [y]
+            else:
+                dp_dict[y+x][0].append(x)
+                dp_dict[y+x][1].append(y)
+        if (y-x) in dm:
+            if y-x not in dm_dict:
+                dm_dict[y-x] = [[],[]]
+                dm_dict[y-x][0] = [x]
+                dm_dict[y-x][1] = [y]
+            else:
+                dm_dict[y-x][0].append(x)
+                dm_dict[y-x][1].append(y)
+
+    for e in dp_dict:
+        if len(dp_dict[e][0]) == 1:
+            plt.plot(dp_dict[e][0],dp_dict[e][1],'bs')    
+        else:
+            plt.plot(dp_dict[e][0],dp_dict[e][1],'b')
+    
+    for e in dm_dict:
+        if len(dm_dict[e][0]) == 1:
+            plt.plot(dm_dict[e][0],dm_dict[e][1],'rs')
+        else:
+            plt.plot(dm_dict[e][0],dm_dict[e][1],'r')
+
+    # Save file in root directory
+    file_name = f"{ruta}figs/{n}-queens-solution.png"
+    plt.savefig(file_name)
+
+    return file_name
+
+if __name__ == "__main__":
+
+    ruta = 'data/c_data/'
+    # n = int(sys.argv[1])
+    n = 4
+    itr = 100
+    dp = [3]
+    dm = []
+    d = len(dp) + len(dm)
+
+    for ix in range(1):
+        print("Trying to place {n} queens on a {n}*{n} chessboard.".format(n=n))
+        sampleset, sampler, py_time = n_queens(n,dp,dm,itr)
+
+        # Extract run info
+        sample = sampleset.first.sample
+        energy = sampleset.first.energy
+        num_itr = len(sampleset)
+        nvars = len(sample)
+        df = sampleset.to_pandas_dataframe()
+        nsols = df[df["energy"] == -2*n]['num_occurrences'].sum()
+        p_sol = psol = nsols / num_itr
+        sp_name = str(sampler).split(".")[4].split()[0]
+        ID = time()
+        
+        if is_valid_solution(n,sample):
+            solved = "YES"
+        else:
+            solved = "NO"
+
+        # print('Solution\n', sample)
+        print('sampleset.info: ',sampleset.info)
+        print('sampler.properties: ',sampler.properties)
+        print('sampler',sp_name)
+        print('py_time', py_time)
+        print('nsols', nsols)
+        print('num_itr', num_itr)
+        print('p_sol', p_sol)
+        print('energy', energy)
+        print('nvars', nvars)        
+        print('- Solved - ', solved)
+
+        # Write sampleset and solutions to file
+        # f1 = open(f"{ruta}sp/{n}_sols_{ID}.txt", "w")
+        # for spl in sampleset:
+        #     f1.write(str(spl)+'\n')
+        # f1.close()
+
+        # f22 = open(f"{ruta}sp/{n}_samplesetPD_{ID}.txt", "w")
+        # f22.write(df.to_string())
+        # f22.close()
+
+        # f3 = open(f"{ruta}logs_vsH.txt", "a")
+        # f3.write(f'{n}-q; {d}-d config\n')
+        # f3.write(str(sample)+'\n')
+        # f3.write('py_time ' + str(py_time) + '\n')
+        # f3.write(str(sampleset.info)+'\n')
+        # f3.write('Solution - ' + solved + '\n\n')
+        # f3.close()
+
+        line = f'{n}   {d}   {nvars}   {num_itr}   {p_sol}   {sp_name}   '\
+               f'{py_time*10**3}   {energy}   {solved}\n'
+        f4 = open(f"{ruta}time.txt", "a")
+        f4.write(line)
+        f4.close()
+
+        # f2 = open(f"{ruta}sp/{n}_sampleset_{ID}.txt", "w")
+        # f2.write(str(sampleset))
+        # f2.close()
+
+        file_name = plot_chessboard(n,sample,dp,dm,ruta)
+        print("Chessboard created. See: {}".format(file_name))
---
+++ n_queens10Q_rBQM.py
@@ -0,0 +1,346 @@
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+#
+# 
+# N Queen Solution using cell's row & columns
+# 
+# - block diagonals
+# - solution verifier
+# - draw board
+
+from collections import Counter
+
+import numpy as np
+import matplotlib
+matplotlib.use("agg")    # must select backend before importing pyplot
+import matplotlib.pyplot as plt
+from dimod import BinaryQuadraticModel
+from dwave.system import LeapHybridSampler
+from dwave.system import DWaveSampler, EmbeddingComposite, FixedEmbeddingComposite
+from dwave.system import DWaveCliqueSampler
+import dwave.inspector
+import neal
+from time import time
+import sys
+import datetime as dt
+
+
+def n_queens(n,dp,dm,itr,ruta, sampler=None):
+    """Returns a potential solution to the n-queens problem in a dictionary, where
+    the keys are qubit id and value on or off
+
+    Args:
+        n: Number of queens to place.
+
+        sampler: A binary quadratic model sampler. 
+        Can use: 1) LeapHybridSampler
+                 2) DWaveSampler - QPU
+                 3) SimulatedAnnealingSampler
+    """
+
+    d = len(dp) + len(dm)
+
+    bqm = BinaryQuadraticModel({}, {}, 0, 'BINARY')
+    # bqm.offset = 2*n
+    # itr = 500
+    w = 2
+    # cs = 89
+
+    for i in range(n**2):
+        ri = i // n
+        ci = i-n*ri
+        if (ri+ci) in dp:
+            continue
+        elif (ri-ci) in dm:
+            continue
+        else:
+            bqm.add_variable(i, -w)
+            
+        for j in range(i):
+            if j not in bqm.linear:
+                continue
+            rj = j // n
+            cj = j-n*rj
+            if rj == ri:
+                bqm.add_interaction(i, j, w)
+            if cj == ci:
+                bqm.add_interaction(i, j, w)
+            if abs(ri-rj) == abs(ci-cj):
+                bqm.add_interaction(i, j, w)
+
+    print(f'solver started with {itr} reads...')
+
+    # QPU   
+    start_time = time()
+    sampler = EmbeddingComposite(DWaveSampler())
+    embedding_time = time()-start_time
+
+    sampleset = sampler.sample(bqm, num_reads=itr, label=f'n{n}d{d}time{start_time}')
+    py_time = time()-start_time
+
+    sp_name = str(sampler).split(".")[4].split()[0]
+    print('sampler', sampler)
+    print('sp_name', sp_name)
+
+    # Extract run info
+    embedding = sampleset.info['embedding_context']['embedding']
+    chain_break_method = sampleset.info['embedding_context']['chain_break_method']
+    chain_strength = sampleset.info['embedding_context']['chain_strength']
+    n_vars = len(embedding.keys())
+    n_qb = sum(len(chain) for chain in embedding.values())
+    sample = sampleset.first.sample
+    energy = sampleset.first.energy
+    df = sampleset.to_pandas_dataframe()
+    nsols = df[df["energy"] == -2*n]['num_occurrences'].sum()
+    psol = nsols / itr
+    print('sp_name', sp_name)
+    print(f"Number of logical variables: {n_vars}")
+    print(f"Number of physical qubits used in embedding: {n_qb}")
+    print('energy', energy)
+    print('py_time', py_time)
+    print('nsols', nsols)
+    print('psol', psol)
+    print('warnings', sampleset.info['warnings'])
+
+    f1 = open(f"{ruta}sp/{n}_sols_{start_time}.txt", "w")
+    for spl in sampleset:
+        f1.write(str(spl)+'\n')
+    f1.close()
+
+    f2 = open(f"{ruta}sp/{n}_sampleset_{start_time}.txt", "w")
+    f2.write(str(sampleset))
+    f2.close()
+
+    f22 = open(f"{ruta}sp/{n}_samplesetPD_{start_time}.txt", "w")
+    f22.write(df.to_string())
+    f22.close()    
+
+    f3 = open(f"{ruta}logsQPU.txt", "a")
+    f3.write(f'{n}-q; {d}-d config\n')
+    f3.write(f'itr: {itr}\n')
+    f3.write('sp_name ' + str(sp_name) + '\n')
+    f3.write('embedding_time ' + str(embedding_time) + '\n')
+    f3.write(str(sample)+'\n')
+    f3.write('py_time ' + str(py_time) + '\n')
+    f3.write(str(sampleset.info)+'\n')
+    f3.write(str(sampleset.info['warnings'])+'\n')
+    f3.close()
+
+    f4 = open(f"{ruta}time.txt", "a")
+    line = f'{n}   {d}   {itr}   {sp_name}   {psol}   {energy}   {n_vars}   {n_qb}   ' \
+           f'{chain_strength}   {chain_break_method}   {py_time*10**3}   '
+    line += f'{embedding_time*10**3}   ' if embedding_time else f'NA   '
+    for v in sampleset.info["timing"]:
+        line = line + '   ' + str(sampleset.info["timing"][v]*10**-3)
+    f4.write(line + '\n')
+    f4.close()
+    
+    dwave.inspector.show(sampleset)             # Show Dwave Inspector Tool
+    return sample,dp,dm
+
+
+def is_valid_solution(n, solution):
+    """Check that solution is valid by making sure all constraints were met.
+
+    Args:
+        n: Number of queens in the problem.
+
+        solution: A dictionary of qubits, key = qubit id, value = 0 or 1
+    """
+    board = np.zeros((n,n))
+    ldp = []                            # Keep track of queens on D+
+    ldm = []                            # Keep track of queens on D-
+
+    for qb,v in solution.items():       # Build & Check diag/anti-diag constraints
+        r = qb // n
+        c = qb-n*r
+        if v:
+            board[r,c] = v
+            dp = r+c
+            dm = r-c
+            if dp not in ldp:
+                ldp.append(dp)
+            else:
+                print(f"D+ diagonal {dp} has more than 1 queen")
+                return False
+            if dm not in ldm:
+                ldm.append(dm)
+            else:
+                print(f"D- diagonal {dm} has more than 1 queen")
+                return False
+
+    for i in range(n):                              # Check row/col constraints
+        sum_row = sum(board[i,:])
+        if sum_row != 1:
+            print(f"Row {i} has {sum_row} queens.")
+            return False
+        sum_col = sum(board[:,i])
+        if sum_col != 1:
+            print(f"Column {i} has {sum_col} queens.")
+            return False
+
+    return True
+
+
+def plot_chessboard(n,s,dp,dm,ruta):
+    """Create a chessboard with queens using matplotlib. Image is saved
+    in the root directory. Returns the image file name.
+    """
+    chessboard = np.zeros((n,n))
+    chessboard[1::2,0::2] = 1
+    chessboard[0::2,1::2] = 1
+
+    # Adjust fontsize for readability
+    if n <= 10:
+        fontsize = 30
+    elif n <= 20:
+        fontsize = 10
+    else:
+        fontsize = 5
+
+    plt.xticks(np.arange(n))
+    plt.yticks(np.arange(n))
+
+    plt.imshow(chessboard, cmap='binary')
+
+    # Place queens
+    for qb,v in s.items():
+        y = qb // n
+        x = qb-n*y
+        if v:
+            plt.text(x, y, u"\u2655", fontsize=fontsize, ha='center',
+                        va='center', color='black' if (x - y) % 2 == 0 else 'white')
+
+    # Draw blocked diagonals
+    dp_dict = {}
+    dm_dict = {}
+    for i in range(n**2):
+        y = i // n
+        x = i-n*y
+        if (y+x) in dp:
+            if y+x not in dp_dict:
+                dp_dict[y+x] = [[],[]]
+                dp_dict[y+x][0] = [x]
+                dp_dict[y+x][1] = [y]
+            else:
+                dp_dict[y+x][0].append(x)
+                dp_dict[y+x][1].append(y)
+        if (y-x) in dm:
+            if y-x not in dm_dict:
+                dm_dict[y-x] = [[],[]]
+                dm_dict[y-x][0] = [x]
+                dm_dict[y-x][1] = [y]
+            else:
+                dm_dict[y-x][0].append(x)
+                dm_dict[y-x][1].append(y)
+
+    for e in dp_dict:
+        if len(dp_dict[e][0]) == 1:
+            plt.plot(dp_dict[e][0],dp_dict[e][1],'bs')    
+        else:
+            plt.plot(dp_dict[e][0],dp_dict[e][1],'b')
+    
+    for e in dm_dict:
+        if len(dm_dict[e][0]) == 1:
+            plt.plot(dm_dict[e][0],dm_dict[e][1],'rs')
+        else:
+            plt.plot(dm_dict[e][0],dm_dict[e][1],'r')
+    
+    d = len(dp) + len(dm)
+    # Save file in root directory
+    file_name = f"{ruta}figs/n{n}-d{d}queens-solution.png"
+    plt.savefig(file_name)
+
+    return file_name
+
+if __name__ == "__main__":
+
+    ruta = 'data/q_data/'
+    # n = int(sys.argv[1])
+    n = 11
+    itr = 3000
+
+    # dp = []
+    # dm = []
+
+    # Diags n 7
+    # dp = [9]
+    # dm = []
+
+    # dp = [11]
+    # dm = [3]
+
+    # dp = [9]
+    # dm = [4,3]
+
+    # dp = [11,2,8]
+    # dm = [-6]
+
+    # dp = [2,1,10,11]
+    # dm = [-4]
+
+    # dp = [11,2]
+    # dm = [6,-5,-6,4]
+
+    # dp = [6,5,12]
+    # dm = [6,5,-4,-6]
+
+    # dp = [1,2,12,10]
+    # dm = [2,-2,-6,6]
+
+
+    # Diags n 8
+    # dp = [14,8,7,0,6]
+    # dm = [7,-7,6,-6,5,-5]
+
+
+    # Diags n 9
+    # dp = [0, 12, 4, 16, 1, 15, 14]
+    # dm = [-7, 4, 0, -4, 1, 8]
+
+
+    # Diags n 10
+    # dp = [1, 3, 2, 12, 15, 0, 18]
+    # dm = [6, -8, 1, -7, 3, 7]
+
+    # dp = [18, 14, 3, 11, 12, 1, 0, 5]
+    # dm = [-9, -5, 4, -7, 8, 1]
+
+    # dp = [18, 16, 2, 0, 17, 7, 3]
+    # dm = [7, -7, -2, -8, 9, 8, 0, 2, -9]
+
+
+    # Diags n 11
+    dp = [1, 15, 2, 20, 7, 11, 10, 16]
+    dm = [4, -4, -7, -10, 9, -9, 8, 10, 7]
+    
+
+
+    print("Trying to place {n} queens on a {n}*{n} chessboard.".format(n=n))
+    s,dp,dm = n_queens(n,dp,dm,itr,ruta)
+
+    if is_valid_solution(n,s):
+        write = "Solution - VALID"
+    else:
+        write = "Solution - NOT VALID"
+    print(write)
+
+    f = open(f"{ruta}logsQPU.txt", "a")
+    f.write(write +'\n\n')
+    f.close()
+
+    file_name = plot_chessboard(n,s,dp,dm,ruta)
+    print("Chessboard created. See: {}".format(file_name))
